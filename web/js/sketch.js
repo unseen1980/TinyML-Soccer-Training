cca15c3c1d800ddc1cfe7a5ac5b755219af14959
Christos Koutsiaris <christos.koutsiaris@sap.com>
2021-06-29 00:20:08 +0100

    Comment redundant code

--- web/js/sketch.js
+++ web/js/sketch.js
@@ -12,219 +12,219 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let r = 0;
-let g = 0;
-let b = 0;
-let x;
-let y;
-let d = 0;
-let wave_text = 10;
-let sc = 0;
-let leftSound, rightSound, poke, pluck, twirl, D1, D2, D3, D4;
-let drums = [];
+// let r = 0;
+// let g = 0;
+// let b = 0;
+// let x;
+// let y;
+// let d = 0;
+// let wave_text = 10;
+// let sc = 0;
+// let leftSound, rightSound, poke, pluck, twirl, D1, D2, D3, D4;
+// let drums = [];
 let gesture;
 
 const GESTURES = ["left", "right", "poke", "twirl", "pluck"];
 
-function preload() {
-  leftSound = loadSound("./sounds/left.wav");
-  rightSound = loadSound("./sounds/right.wav");
-  twirl = loadSound("./sounds/twirl.wav");
-  poke = loadSound("./sounds/poke.wav");
-  pluck = loadSound("./sounds/pluck.wav");
-  D1 = loadSound("./sounds/D1.wav");
-  D2 = loadSound("./sounds/D2.wav");
-  D3 = loadSound("./sounds/D3.wav");
-  D4 = loadSound("./sounds/D4.wav");
-}
+// function preload() {
+//   leftSound = loadSound("./sounds/left.wav");
+//   rightSound = loadSound("./sounds/right.wav");
+//   twirl = loadSound("./sounds/twirl.wav");
+//   poke = loadSound("./sounds/poke.wav");
+//   pluck = loadSound("./sounds/pluck.wav");
+//   D1 = loadSound("./sounds/D1.wav");
+//   D2 = loadSound("./sounds/D2.wav");
+//   D3 = loadSound("./sounds/D3.wav");
+//   D4 = loadSound("./sounds/D4.wav");
+// }
 
 function setup() {
   // var canvas = createCanvas(windowWidth, windowHeight);
   // canvas.parent("sketch-holder");
-  drums = [D1, D2, D3, D4];
+  // drums = [D1, D2, D3, D4];
 }
 
-function draw() {
-  background(r, g, b, 5);
-  noStroke();
+// function draw() {
+//   background(r, g, b, 5);
+//   noStroke();
 
-  push();
-  fill(255, wave_text);
-  textSize(20);
-  textFont("Roboto+Mono");
-  text(" wave your finger ", windowWidth / 2 - 20, windowHeight / 2);
-  pop();
-}
+//   push();
+//   fill(255, wave_text);
+//   textSize(20);
+//   textFont("Roboto+Mono");
+//   text(" wave your finger ", windowWidth / 2 - 20, windowHeight / 2);
+//   pop();
+// }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
 
-//LEFT KEY
-function left_gesture(total) {
-  r = 23;
-  g = 114;
-  b = 250;
-  sc = 255;
-  wave_text = 0;
+// //LEFT KEY
+// function left_gesture(total) {
+//   r = 23;
+//   g = 114;
+//   b = 250;
+//   sc = 255;
+//   wave_text = 0;
 
-  y = windowHeight / 2;
-  push();
-  fill(224, 155, 11);
-  ellipse(x, y - 300, 300, 300);
-  ellipse(x, y, 150);
-  ellipse(x, y + 300, 300, 300);
-  pop();
+//   y = windowHeight / 2;
+//   push();
+//   fill(224, 155, 11);
+//   ellipse(x, y - 300, 300, 300);
+//   ellipse(x, y, 150);
+//   ellipse(x, y + 300, 300, 300);
+//   pop();
 
-  leftSound.play();
-  push();
-  textSize(16);
-  fill(255);
-  textFont("Roboto+Mono");
-  text(`L E F T : ${total}`, 20, 20);
-  pop();
-}
+//   leftSound.play();
+//   push();
+//   textSize(16);
+//   fill(255);
+//   textFont("Roboto+Mono");
+//   text(`L E F T : ${total}`, 20, 20);
+//   pop();
+// }
 
-//RIGHT KEY
-function right_gesture(total) {
-  r = 224;
-  g = 155;
-  b = 11;
-  sc = 255;
-  wave_text = 0;
-  y = windowHeight / 2;
+// //RIGHT KEY
+// function right_gesture(total) {
+//   r = 224;
+//   g = 155;
+//   b = 11;
+//   sc = 255;
+//   wave_text = 0;
+//   y = windowHeight / 2;
 
-  push();
-  fill(195, 184, 245);
-  ellipse(x - 300, y, 100, 100);
-  ellipse(x - 150, y, 100, 100);
-  ellipse(x, y, 100, 100);
-  ellipse(x + 150, y, 100, 100);
-  ellipse(x + 300, y, 100, 100);
-  pop();
-  rightSound.play();
+//   push();
+//   fill(195, 184, 245);
+//   ellipse(x - 300, y, 100, 100);
+//   ellipse(x - 150, y, 100, 100);
+//   ellipse(x, y, 100, 100);
+//   ellipse(x + 150, y, 100, 100);
+//   ellipse(x + 300, y, 100, 100);
+//   pop();
+//   rightSound.play();
 
-  push();
-  textSize(16);
-  fill(255);
-  textFont("Roboto+Mono");
-  text(`R I G H T : ${total}`, 20, 40);
-  pop();
-}
+//   push();
+//   textSize(16);
+//   fill(255);
+//   textFont("Roboto+Mono");
+//   text(`R I G H T : ${total}`, 20, 40);
+//   pop();
+// }
 
-//DOWN KEY
-function poke_gesture(total) {
-  r = 195;
-  g = 184;
-  b = 245;
-  sc = 255;
-  wave_text = 0;
-  x = windowWidth / 2;
-  push();
-  fill(134, 224, 163);
-  ellipse(x, y, 600, 600);
-  pop();
+// //DOWN KEY
+// function poke_gesture(total) {
+//   r = 195;
+//   g = 184;
+//   b = 245;
+//   sc = 255;
+//   wave_text = 0;
+//   x = windowWidth / 2;
+//   push();
+//   fill(134, 224, 163);
+//   ellipse(x, y, 600, 600);
+//   pop();
 
-  push();
-  fill(195, 184, 245);
-  ellipse(x, y, 500, 500);
-  pop();
+//   push();
+//   fill(195, 184, 245);
+//   ellipse(x, y, 500, 500);
+//   pop();
 
-  push();
-  fill(134, 224, 163);
-  ellipse(x, y, 400, 400);
-  pop();
+//   push();
+//   fill(134, 224, 163);
+//   ellipse(x, y, 400, 400);
+//   pop();
 
-  push();
-  fill(195, 184, 245);
-  ellipse(x, y, 300, 300);
-  pop();
+//   push();
+//   fill(195, 184, 245);
+//   ellipse(x, y, 300, 300);
+//   pop();
 
-  push();
-  fill(134, 224, 163);
-  ellipse(x, y, 200, 200);
-  pop();
-  poke.play();
+//   push();
+//   fill(134, 224, 163);
+//   ellipse(x, y, 200, 200);
+//   pop();
+//   poke.play();
 
-  push();
-  textSize(16);
-  fill(255);
-  textFont("Roboto+Mono");
-  text(`P O K E : ${total}`, 20, 60);
-  pop();
-}
+//   push();
+//   textSize(16);
+//   fill(255);
+//   textFont("Roboto+Mono");
+//   text(`P O K E : ${total}`, 20, 60);
+//   pop();
+// }
 
-//A KEY
-function twirl_gesture(total) {
-  r = 134;
-  g = 224;
-  b = 163;
-  sc = 255;
-  wave_text = 0;
+// //A KEY
+// function twirl_gesture(total) {
+//   r = 134;
+//   g = 224;
+//   b = 163;
+//   sc = 255;
+//   wave_text = 0;
 
-  x = windowWidth / 2;
+//   x = windowWidth / 2;
 
-  push();
-  fill(g, b, b);
-  ellipse(x, y, 900);
-  pop();
-  push();
-  fill(250, 75, 67);
-  ellipse(x - x / 2, y - y / 2, 200, 200);
-  ellipse(x - x / 2, y + y / 2, 400, 400);
-  ellipse(x + x / 2, y + y / 2, 200, 200);
-  ellipse(x + x / 2, y - y / 2, 400, 400);
-  pop();
+//   push();
+//   fill(g, b, b);
+//   ellipse(x, y, 900);
+//   pop();
+//   push();
+//   fill(250, 75, 67);
+//   ellipse(x - x / 2, y - y / 2, 200, 200);
+//   ellipse(x - x / 2, y + y / 2, 400, 400);
+//   ellipse(x + x / 2, y + y / 2, 200, 200);
+//   ellipse(x + x / 2, y - y / 2, 400, 400);
+//   pop();
 
-  twirl.play();
+//   twirl.play();
 
-  push();
-  textSize(16);
-  fill(255);
-  textFont("Roboto+Mono");
-  text(`T W I R L : ${total}`, 20, 80);
-  pop();
-}
+//   push();
+//   textSize(16);
+//   fill(255);
+//   textFont("Roboto+Mono");
+//   text(`T W I R L : ${total}`, 20, 80);
+//   pop();
+// }
 
-//UP KEY
-function pluck_gesture(total) {
-  r = 250;
-  g = 75;
-  b = 67;
-  wave_text = 0;
-  sc = 255;
+// //UP KEY
+// function pluck_gesture(total) {
+//   r = 250;
+//   g = 75;
+//   b = 67;
+//   wave_text = 0;
+//   sc = 255;
 
-  x = windowWidth / 2;
-  push();
-  fill(23, 114, 250);
-  ellipse(x - x / 2, y, 500, 500);
-  ellipse(x + x / 2, y, 500, 500);
-  pop();
+//   x = windowWidth / 2;
+//   push();
+//   fill(23, 114, 250);
+//   ellipse(x - x / 2, y, 500, 500);
+//   ellipse(x + x / 2, y, 500, 500);
+//   pop();
 
-  push();
-  fill(r, g, b);
-  ellipse(x - x / 2, y - y / 4, 200);
-  ellipse(x + x / 2, y + y / 4, 200);
-  pop();
-  pluck.play();
+//   push();
+//   fill(r, g, b);
+//   ellipse(x - x / 2, y - y / 4, 200);
+//   ellipse(x + x / 2, y + y / 4, 200);
+//   pop();
+//   pluck.play();
 
-  if (drums[d].isPlaying()) {
-    drums[d].stop();
-  }
-  d = d + 1;
-  if (d > drums.length - 1) {
-    d = 0;
-  }
+//   if (drums[d].isPlaying()) {
+//     drums[d].stop();
+//   }
+//   d = d + 1;
+//   if (d > drums.length - 1) {
+//     d = 0;
+//   }
 
-  drums[d].loop();
+//   drums[d].loop();
 
-  push();
-  textSize(16);
-  fill(255);
-  textFont("Roboto+Mono");
-  text(`P L U C K : ${total}`, 20, 100);
-  pop();
-}
+//   push();
+//   textSize(16);
+//   fill(255);
+//   textFont("Roboto+Mono");
+//   text(`P L U C K : ${total}`, 20, 100);
+//   pop();
+// }
 
 let count = {
   left: 0,
@@ -248,33 +248,33 @@ function onInferenceSketch(i) {
       gesture = "left";
       console.log("Gesture: ", gesture);
       updateCount(gesture);
-      left_gesture(count[gesture]);
+      // left_gesture(count[gesture]);
 
       break;
     case 1:
       gesture = "right";
       console.log("Gesture: ", gesture);
       updateCount(gesture);
-      right_gesture(count[gesture]);
+      // right_gesture(count[gesture]);
 
       break;
     case 2:
       gesture = "poke";
       console.log("Gesture: ", gesture);
       updateCount(gesture);
-      poke_gesture(count[gesture]);
+      // poke_gesture(count[gesture]);
       break;
     case 3:
       gesture = "twirl";
       console.log("Gesture: ", gesture);
       updateCount(gesture);
-      twirl_gesture(count[gesture]);
+      // twirl_gesture(count[gesture]);
       break;
     case 4:
       gesture = "pluck";
       console.log("Gesture: ", gesture);
       updateCount(gesture);
-      pluck_gesture(count[gesture]);
+      // pluck_gesture(count[gesture]);
 
       break;
     default:
@@ -283,31 +283,31 @@ function onInferenceSketch(i) {
 }
 
 //Try application with keyboard
-function keyPressed() {
-  //RIGHT
-  if (keyCode == "39") {
-    right_gesture();
-  }
-  //LEFT
-  else if (keyCode == "37") {
-    left_gesture();
-  }
+// function keyPressed() {
+//   //RIGHT
+//   if (keyCode == "39") {
+//     right_gesture();
+//   }
+//   //LEFT
+//   else if (keyCode == "37") {
+//     left_gesture();
+//   }
 
-  //UP - PLUCK
-  else if (keyCode == "38") {
-    pluck_gesture();
-  }
+//   //UP - PLUCK
+//   else if (keyCode == "38") {
+//     pluck_gesture();
+//   }
 
-  //DOWN -- poke
-  else if (keyCode == "40") {
-    poke_gesture();
-  }
+//   //DOWN -- poke
+//   else if (keyCode == "40") {
+//     poke_gesture();
+//   }
 
-  //A -- TWIRL
-  else if (keyCode == "65") {
-    twirl_gesture();
-  }
-}
+//   //A -- TWIRL
+//   else if (keyCode == "65") {
+//     twirl_gesture();
+//   }
+// }
 
 /************************************************************************
 // SCRIPT FOR DISPLAY
@@ -363,11 +363,11 @@ window.tinyMlExperimentBleInterface.createConnectButton(
     },
 
     onTransferCompleted() {
-      setTimeout(() => {
-        document.querySelector(".play").scrollIntoView({
-          behavior: "smooth",
-        });
-      }, 100);
+      // setTimeout(() => {
+      //   document.querySelector(".play").scrollIntoView({
+      //     behavior: "smooth",
+      //   });
+      // }, 100);
     },
 
     onDisconnect() {
