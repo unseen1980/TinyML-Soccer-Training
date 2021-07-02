63e6f794fec5bd633a105d377c043cacb0ccac9f
Christos Koutsiaris <christos.koutsiaris@sap.com>
2021-07-02 09:25:17 +0100

    Minor wording changes

--- web/js/app.js
+++ web/js/app.js
@@ -19,7 +19,7 @@ class LikeButton extends React.Component {
         onClick: () => this.setState({ liked: true }),
         className: "btn",
       },
-      "Record"
+      "Save"
     );
     // return <button onClick={() => this.setState({ liked: true })}>Like</button>;
   }
