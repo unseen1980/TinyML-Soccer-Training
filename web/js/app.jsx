0a83b66ab22cd0214fed2ea6d07f5f2e9d9115a5
Christos Koutsiaris <christos.koutsiaris@sap.com>
2021-07-02 16:20:55 +0100

    Add babel script for jsx. Needs cleanup. Sleepless update just to work

---
+++ web/js/app.jsx
@@ -0,0 +1,20 @@
+"use strict";
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return "You liked this.";
+    }
+    return <button className="btn btn--primary" onClick={() => this.setState({ liked: true })}>Save stats</button>;
+  }
+}
+
+const domContainer = document.querySelector("#reactBtn");
+ReactDOM.render(e(LikeButton), domContainer);
